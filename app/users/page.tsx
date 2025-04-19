6bafd6f8c2513c85c712712d222be56a187ae15a
yamchips <wflunch@gmail.com>
2025-04-19 14:52:32 +0100

    Caching, static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -12,12 +12,20 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+    // default setting is cache: 'force-cache'
+    // { cache: "no-store" }, // don't store data in cache
+    // { cache: "no-cache" }, // use cache, but check data validity before using it
+    // { next: { revalidate: 10 }, // Cache: refresh every 10 seconds, only in fetch function, axios doesn't have this
+    // }
+  );
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
