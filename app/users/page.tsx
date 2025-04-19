b72eebe6396643a6cb30a8bacc91e6b7348db7f3
yamchips <wflunch@gmail.com>
2025-04-19 14:08:23 +0100

    Add address field to User interface

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface Address {
+  street: string;
+  city: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  address: Address;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} lives on {user.address.street} in {user.address.city}.
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
