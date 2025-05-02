0ef7124f208aa58092aea8137495bbd7345c4c29
yamchips <wflunch@gmail.com>
2025-05-02 14:46:03 +0100

    Comment send-email route.ts

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -3,14 +3,14 @@ import WelcomeTemplate from "@/emails/WelcomeTemplate";
 import { NextResponse } from "next/server";
 import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  await resend.emails.send({
-    from: "mydomain.com", // from a domain that I own, cannot use free services like Gmail
-    to: "wflunch@gmail.com",
-    subject: "new subject",
-    react: React.createElement(WelcomeTemplate, { name: "harry" }),
-  });
-  return NextResponse.json({ message: "Email send successfully." });
-}
+// export async function POST() {
+//   await resend.emails.send({
+//     from: "mydomain.com", // from a domain that I own, cannot use free services like Gmail
+//     to: "wflunch@gmail.com",
+//     subject: "new subject",
+//     react: React.createElement(WelcomeTemplate, { name: "harry" }),
+//   });
+//   return NextResponse.json({ message: "Email send successfully." });
+// }
